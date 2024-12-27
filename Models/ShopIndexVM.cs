3674657aa5c6fb96f213b07249a09eeca538af6d
Esra Sergiser <128863910+EsraSrg@users.noreply.github.com>
2024-12-27 16:08:43 +0300

    Shop Filter tamamlandı

--- Models/ShopIndexVM.cs
+++ Models/ShopIndexVM.cs
@@ -6,7 +6,12 @@ public class ShopIndexVM
     public List<CategoryVM> Categories { get; set; }
     public List<SubCategoryVM> SubCategories { get; set; }
 
-     public decimal MinPrice { get; set; }
+    public decimal MinPrice { get; set; }
     public decimal MaxPrice { get; set; }
     public List<string> Colors { get; set; }
+
+    public decimal? SelectedMinPrice { get; set; }
+    public decimal? SelectedMaxPrice { get; set; }
+    public List<string> SelectedColors { get; set; }
+
 }
